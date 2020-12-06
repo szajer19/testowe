e09ab83def6048fc2f4c53ecb15e0a378a05a6ff
Mateusz <mateuszszajerski@gmail.com>
2020-12-06 03:20:49 +0100

    Changed in index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <body>
     <h1>
         strona startowa 
+        <h3>Zmiany numer 1</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dicta omnis hic. Necessitatibus ipsum sunt maiores, ea esse quidem consectetur nobis id neque tempore dolorum. Quidem optio, rerum doloremque alias consequuntur ipsam recusandae, voluptatem suscipit perspiciatis molestiae consequatur ut! Exercitationem, obcaecati. Consectetur quo necessitatibus laboriosam harum labore iure nesciunt tempora velit? Aperiam veritatis rem atque, deleniti harum commodi culpa magni debitis alias minus soluta quaerat labore officiis dolore sint. Fuga iusto labore accusamus ut repudiandae accusantium molestiae magni error dicta. Autem dicta minima doloribus quod sunt voluptatibus fugit ex consequuntur optio architecto nobis corporis ipsam similique ut enim voluptatum saepe repellat eos odit quidem, quisquam officia esse nam facilis! Rem quibusdam alias corporis! Consequuntur, sit ullam! Recusandae vel eveniet ipsam id facilis facere odio harum reiciendis? Asperiores possimus placeat nobis quasi cumque quaerat laborum ipsa rem sint sapiente illum iusto sunt velit fugiat doloribus ullam, molestiae nesciunt est ipsam! Minus consequatur facilis aut. Vel, error dignissimos ex deleniti corporis fugit laboriosam, velit cupiditate iure beatae vitae laborum modi provident consectetur illo perferendis dolore sed consequuntur officia minus quo? Aspernatur ea similique odit architecto provident omnis voluptate dolores aliquid dolor consectetur. Perspiciatis repellendus veritatis harum inventore laboriosam, numquam voluptatem nostrum impedit!
+        
     </h1>
 </body>
 </html>
